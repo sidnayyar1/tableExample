347de21dd38061cd75fe40de867ef25a2c6d5312
sidharth nayyar <sidnayyar1@gmail.com>
2019-11-10 17:52:14 -0500

    [sidharth] Simple tableview example

--- tableExample/ViewController.swift
+++ tableExample/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
 
+    private var data:[String] = []
+    @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        myTableView.dataSource = self
+        for i in 0...1000{
+        data.append("\(i)")
+        }
         // Do any additional setup after loading the view.
     }
-
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "cellIdentifier")!
+        let text = data[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
 }
 
